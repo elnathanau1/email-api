20073c94c32a3a477a9c860b39ee1c2bbe6bcfc6
elnathanau1 <elnathanau1@gmail.com>
2020-06-03 11:27:41 -0500

    spring initalzr

---
+++ src/main/java/eau/projects/nooliganscraper/NooliganScraperApplication.java
@@ -0,0 +1,13 @@
+package eau.projects.nooliganscraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NooliganScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NooliganScraperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eau/projects/nooliganscraper/NooliganScraperApplicationTests.java
@@ -0,0 +1,13 @@
+package eau.projects.nooliganscraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NooliganScraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
