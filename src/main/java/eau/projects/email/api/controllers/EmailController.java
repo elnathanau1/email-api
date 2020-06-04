1734ded1250e51b628d05078f05a0bacad6eb889
ElnathanAu <elnathanau1@gmail.com>
2020-06-04 16:55:29 -0500

    remove logger

--- src/main/java/eau/projects/email/api/controllers/EmailController.java
+++ src/main/java/eau/projects/email/api/controllers/EmailController.java
@@ -13,8 +13,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class EmailController {
@@ -24,18 +22,13 @@ public class EmailController {
     @Value("${controllers.password}")
     private String controllersPassword;
 
-    Logger logger = LoggerFactory.getLogger(EmailController.class);
-
     @PostMapping("/email")
     public ResponseEntity sendEmail(@RequestBody EmailRequest emailRequest, @RequestHeader String authorization) {
         if (authorization.equals(controllersPassword)) {
-            logger.info("{}, {}, {}", emailRequest.getTo(), emailRequest.getSubject(), emailRequest.getMessage());
             emailService.sendSimpleMessage(emailRequest.getTo(), emailRequest.getSubject(), emailRequest.getMessage());
-            logger.info("fine");
             return new ResponseEntity(HttpStatus.ACCEPTED);
-        }
-        else {
-            throw new UnauthorizedException();
+        } else {
+            return new ResponseEntity(HttpStatus.UNAUTHORIZED);
         }
     }
 }
@@ -47,6 +40,3 @@ class EmailRequest {
     private String subject;
     private String message;
 }
-
-@ResponseStatus(HttpStatus.UNAUTHORIZED)
-class UnauthorizedException extends RuntimeException {}
