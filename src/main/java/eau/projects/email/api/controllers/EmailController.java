898c0975f8f3529afb19aa212a6ec56219ee0b8f
ElnathanAu <elnathanau1@gmail.com>
2020-06-04 16:54:30 -0500

    fix status codes

--- src/main/java/eau/projects/email/api/controllers/EmailController.java
+++ src/main/java/eau/projects/email/api/controllers/EmailController.java
@@ -7,10 +7,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class EmailController {
@@ -23,21 +27,16 @@ public class EmailController {
     Logger logger = LoggerFactory.getLogger(EmailController.class);
 
     @PostMapping("/email")
-    String sendEmail(@RequestBody EmailRequest emailRequest, @RequestHeader String authorization) {
-        try {
-            if (authorization.equals(controllersPassword)) {
-                logger.info("{}, {}, {}", emailRequest.getTo(), emailRequest.getSubject(), emailRequest.getMessage());
-                emailService.sendSimpleMessage(emailRequest.getTo(), emailRequest.getSubject(), emailRequest.getMessage());
-                return "Success";
-            }
-            else {
-                return "Unauthorized";
-            }
-
-        } catch (Exception e) {
-            return "Error: " + e.getMessage();
+    public ResponseEntity sendEmail(@RequestBody EmailRequest emailRequest, @RequestHeader String authorization) {
+        if (authorization.equals(controllersPassword)) {
+            logger.info("{}, {}, {}", emailRequest.getTo(), emailRequest.getSubject(), emailRequest.getMessage());
+            emailService.sendSimpleMessage(emailRequest.getTo(), emailRequest.getSubject(), emailRequest.getMessage());
+            logger.info("fine");
+            return new ResponseEntity(HttpStatus.ACCEPTED);
         }
-
+        else {
+            throw new UnauthorizedException();
+        }
     }
 }
 
@@ -48,3 +47,6 @@ class EmailRequest {
     private String subject;
     private String message;
 }
+
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
+class UnauthorizedException extends RuntimeException {}
